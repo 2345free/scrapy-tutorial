dc35689db8fb1a7e8b7a235c93bf402bfce69af2
luoxiaoxiao <lxx0t01@sp.noahgroup.com>
2019-10-15 17:04:08 +0800

    使用管道

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -4,8 +4,26 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class JsonWriterPipeline(object):
+
+    def __init__(self):
+        self.file = open('items.json', 'w')
+
+    def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
